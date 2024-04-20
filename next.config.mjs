1528e03f393df54a09ccb428281b2fdfd85d7c0e
Stern <128687243+Hosi121@users.noreply.github.com>
2024-04-20 20:34:58 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} */
+/**
+* @type {import('next').NextConfig}
+*/
 const nextConfig = {
-    output: "export",
-};
+ output: "export",
+}
 
-module.exports = nextConfig;
+export default nextConfig
