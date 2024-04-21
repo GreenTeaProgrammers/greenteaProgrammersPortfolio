bd7c2beeec0a6375429bb5813672ce84a86320d3
KikyoNanakusa <nanakusa.vrc@gmail.com>
2024-04-21 23:18:26 +0900

    content: Organizationのリンクを追加

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,15 +1,17 @@
 import SectionTitle from "@/components/atoms/sectionTitle";
 import SectionContent from "@/components/molecules/sectionContent";
-import { VStack } from "@chakra-ui/layout";
+import { VStack, Text} from "@chakra-ui/layout";
 import React from "react";
 
 const ContactPage: React.FC = () => {
-  const content: string = `お問い合わせは以下のメールアドレスにお願いします。\nmail: greenteaprogrammers@gmail.com`;
+  const content: string = `お問い合わせは以下にお願いします。
+mail: greenteaprogrammers@gmail.com`
   return (
     <VStack width="100%" p={5}>
       <VStack width="80%" p={5} align="left">
         <SectionTitle title="Contact" />
         <SectionContent content={content} />
+        <Text color="blue.300"><a href="https://github.com/GreenTeaProgrammers">Github Organization</a></Text>
       </VStack>
     </VStack>
   );
